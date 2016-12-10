99a29072dcd10f8d8c103d1de8607a3f05c54b4b
tterrag1098 <tterrag1098@gmail.com>
2016-12-09 19:21:40 -0500

    Day 9

--- src/com/tterrag/advent2016/Day09.java
+++ src/com/tterrag/advent2016/Day09.java
@@ -12,27 +12,43 @@ public class Day09 {
     
     public static void main(String[] args) throws IOException {
         String input = Files.readAllLines(Paths.get("day9.txt")).get(0);
+        String uncompressed = input;
 
         // Part 1 - Actually decompress the string
-        Matcher matcher = MARKER_PATTERN.matcher(input);
+        Matcher matcher = MARKER_PATTERN.matcher(uncompressed);
         int start = 0;
         while (matcher.find(start)) {
             String[] instruction = matcher.group(1).split("x");
             int chars = Integer.parseInt(instruction[0]);
             int repeat = Integer.parseInt(instruction[1]);
-            String toRepeat = input.substring(matcher.end(), matcher.end() + chars);
+            String toRepeat = uncompressed.substring(matcher.end(), matcher.end() + chars);
             String replacement = toRepeat;
             for (int i = 1; i < repeat; i++) {
                 replacement += toRepeat;
             }
-            input = input.replace(matcher.group(0) + toRepeat, replacement);
-            start = input.indexOf(replacement, start) + replacement.length() - 1;
-            matcher.reset(input);
+            uncompressed = uncompressed.replace(matcher.group(0) + toRepeat, replacement);
+            start = uncompressed.indexOf(replacement, start) + replacement.length() - 1;
+            matcher.reset(uncompressed);
         }        
-        System.out.println(input.length());
+        System.out.println("Part 1: " + input.length());
         
         // Part 2 - Calculate it recursively
-        // TODO lol
+        System.out.println("Part 2: " + getUncompressedLength(matcher.reset(input), 0, input.length()));
     }
 
+    private static long getUncompressedLength(Matcher matcher, int start, int end) {
+        long size = 0;
+        matcher.find();
+        try {
+            while (matcher.end() < end) {
+                String[] instruction = matcher.group(1).split("x");
+                int chars = Integer.parseInt(instruction[0]);
+                int repeat = Integer.parseInt(instruction[1]);
+                size += matcher.start() - start;
+                start = matcher.end() + chars;
+                size += getUncompressedLength(matcher, matcher.end(), matcher.end() + chars) * repeat;
+            }
+        } catch (IllegalStateException e) {}
+        return size + (end - start);
+    }
 }
